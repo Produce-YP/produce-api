06b6afbe0a2ba217e6db3eb7793c064d64a9867f
Eugene-Damiani <damianieugene@gmail.com>
2020-11-13 12:58:20 -0600

    changed file

--- app/models/example.js
+++ app/models/example.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 
-const exampleSchema = new mongoose.Schema({
-  title: {
+const vendorSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true
   },
-  text: {
+  address: {
+    type: String,
+    required: true
+  },
+  number: {
     type: String,
     required: true
   },
@@ -18,4 +22,4 @@ const exampleSchema = new mongoose.Schema({
   timestamps: true
 })
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Vendor', vendorSchema)
